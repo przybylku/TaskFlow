29c99a75c1ebfec9f01331e9c7fb63c45e964fac
paprykdev <58005447+paprykdev@users.noreply.github.com>
2023-11-07 15:58:46 +0100

    refactor: same

--- apps/web/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const NoSSR = dynamic(() => import('@/components/dashboard/Dashboard'), { ssr: false })
-export default function DashboardPage(){
-    return (
-        <NoSSR/>
-    )
-}
--- apps/web/store/index.ts
+++
@@ -1,4 +0,0 @@
-import { useDispatch, TypedUseSelectorHook, useSelector} from "react-redux";
-import type {AppDispatch, RootState} from './store'
-export const useAppDispatch: () => AppDispatch =  useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
