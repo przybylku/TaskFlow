27a28734088834427c24ca25bac7b74744c80e95
paprykdev <58005447+paprykdev@users.noreply.github.com>
2023-11-07 15:54:51 +0100

    refactor: fuck next

---
+++ apps/web/src/Components/HomePage.js
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="w-screen h-screen bg-red-700"></div>;
+}
---
+++ apps/web/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./Components";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
